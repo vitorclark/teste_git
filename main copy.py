1c8fc1e0248b8c0810cbbf9e00a0d211baecbc95
JVClarkSM <95631045+JVClarkSM@users.noreply.github.com>
2022-02-26 22:16:50 -0300

    alteracao funcao hello words

---
+++ main copy.py
@@ -0,0 +1,15 @@
+def hello_word:
+    print('Hellow Brasil ')
+
+hello_word()
+
+def atualiza_preco:(preco_antigo)
+    preco_novo = preco_antigo+1
+    return preco_novo
+def sum(b, a):
+    return (a+b)
+
+s = sum(1,2)
+print(s)
+
+atualiza_preco()
